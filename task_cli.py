9cce143937156adabc2c19b7d4cc896a2f9a49de
hanif0526 <hanifabdurrahmanarifai@gmail.com>
2026-03-24 18:53:41 +0700

    Finally done

--- task_cli.py
+++ task_cli.py
@@ -12,8 +12,7 @@ def load_tasks():
     with open(FILE_PATH, 'r') as file:
         try:
             return json.load(file)
-        except:
-            return json.JSONDecodeError
+        except json.JSONDecodeError:
                 return []
                 
 #2. Helper : Save data to the JSON file
@@ -46,14 +45,14 @@ def main():
         save_tasks(tasks)
         print(f"Task added successfuly (ID : {new_task['id']})")
         
-     elif command == "list":
+    elif command == "list":
      	status_filter = sys.argv[2] if len(sys.argv) > 2 else None
      	for t in tasks:
      	    if status_filter is None or t['status'] == status_filter:
      	        print(f"[{t['id']}] {t['description']} - Status : {t['status']} ")
      	        
      	        
-      elif command == "update":
+    elif command == "update":
           task_id = int(sys.argv[2])
           new_desc = " ".join(sys.argv[3:])
           for t in tasks:
@@ -62,6 +61,26 @@ def main():
                   t['updateAt'] = datetime.now().isoformat
           save_tasks(tasks)
           print("Task updated.")
+          
+    elif command == "delete":
+              task_id = int(sys.argv[2])
+              tasks = [t for t in tasks if t['id'] != task_id]
+              save_tasks(tasks)
+              print("Task deleted.")
+              
+    elif command in ["mark-in-progress", "mark-done"]:
+              new_status = "in-progress" in progress if command == "mark-in-progress" else "done"
+              task_id = int(sys.argv[2])
+              for t in tasks:
+                  if t['id'] == task_id:
+                      t['status'] = new_status
+                      t['updateAt'] = datetime.now().isoformat()
+              save_tasks(tasks)
+              print(f"Task marked as {new_status}.")
+              
+              
+if __name__ == "__main__":
+              main()    
               
         
                                                                                                                     
